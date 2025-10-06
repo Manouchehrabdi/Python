697142c6fed1fff57cc07d398bbcb48c0cb93e33
Manouchehrabdi <mabdi0258@gmail.com>
2025-10-06 14:50:13 +0330

    First commit

---
+++ List Comprehension.py
@@ -0,0 +1,3 @@
+numbers = [3, 8, 15, 22, 41, 50, 1.18]
+result = [f"{n} zoj" if n % 2 == 0 else f"{n} Fard" for n in numbers]
+print(result)
---
+++ my_programing.py
@@ -0,0 +1,7 @@
+numbers = [3, 8, 15, 22, 41, 50, 1.18]
+
+for n in numbers:
+    if n % 2 == 0:
+        print(f"{n} zoj")
+    else:
+        print(f"{n} fard")
